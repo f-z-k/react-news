a7627e0e3d2c3ee6bdaab89bf725610bf5c55e6a
zhongkui <fanzhongkui@yuantutech.com>
2018-11-09 08:50:31 +0800

    00

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 // import App from './App';
 import Routers from './routers/index'
 import registerServiceWorker from './registerServiceWorker';
-
+// 666666
 ReactDOM.render(<Routers/>, document.getElementById('root'));
 registerServiceWorker();
